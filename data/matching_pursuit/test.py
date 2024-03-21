fixed some scaling and activation function bugs

data/matching_pursuit/test.py
@@ -37,7 +37,5 @@ class CustomEmbedding(nn.Module):
 # print(embedded.shape)
 a = torch.arange(1*8*12).reshape((1,8,12))
 print(a.cpu().numpy())
-even = a[:,:,::2]
-odd = a[:,:,1::2]
-b = torch.cat((even,odd), dim=2)
-print(b.cpu().numpy())
+a[:,:,2::3] *= 10
+print(a.cpu().numpy())

model_mp.py
@@ -263,7 +263,7 @@ class GPT(nn.Module):
         mag_loss = mag_loss * mask  
         mag_loss = mag_loss.sum() / mask.sum() 
         
-        mask = phase_target > 0.5
+        mask = phase_target == 0.5
         phase_loss = self.circular_loss(phase, phase_target) 
         phase_loss = phase_loss * mask  
         phase_loss = phase_loss.sum() / mask.sum() 
@@ -282,7 +282,6 @@ class GPT(nn.Module):
         device = inputs.device 
         if self.config.conv_input:
             b, t, na, f = inputs.size()
-            pos = torch.arange(0, t, dtype=torch.long, device=device).unsqueeze(1) # shape (t)
             # forward the GPT model itself
             tok_emb = self.transformer.wte(inputs) # token embeddings of shape (b, t, n_embd)
         else:
@@ -290,11 +289,12 @@ class GPT(nn.Module):
             coef = inputs[:,:,self.config.num_atoms:]
             mag = coef[:,:,:self.config.num_atoms]
             b, t, f = idx.size()
-            pos = torch.arange(0, t, dtype=torch.long, device=device).unsqueeze(1) # shape (t)
             idx = torch.clamp(idx, min=0, max=9999)
             tok_emb = self.transformer.wte(idx) # token embeddings of shape (b, t, num_atoms, n_embd)
             weighted_tok_emb = tok_emb *  mag.unsqueeze(-1)
             tok_emb = torch.sum(weighted_tok_emb, dim=2)
+
+        pos = torch.arange(0, t, dtype=torch.long, device=device).unsqueeze(1) # shape (t)
         #this embeds the position in the block
         pos_emb = self.transformer.wpe(pos).squeeze(1) 
         #Expand to match the batch dimension 
@@ -305,25 +305,23 @@ class GPT(nn.Module):
         for block in self.transformer.h:
             x = block(x)
         x = self.transformer.ln_f(x)
-        if targets is not None:
-            #categorical
-            out = self.lm_head(x)
+        loss = None
+        
+        out = self.lm_head(x)
 
-            if self.config.logit_loss:
-                #relu the coeffiecents
-                out[:,:,self.config.vocab_size:] = F.relu(out[:,:,self.config.vocab_size:])
+        if self.config.logit_loss:
+            #relu the coeffiecents
+            out[:,:,self.config.vocab_size:] = F.sigmoid(out[:,:,self.config.vocab_size:])
+            if targets is not None:
                 loss = self.logit_loss(out, targets)
-            #all mse
-            else:
-                b,s,f = out.shape
-                out = out.view(b,s,self.config.num_atoms, self.config.num_features)
-                #relu everything!
-                out = F.relu(out)
-                loss = self.custom_mse_loss(out, targets)
+        #all mse
         else:
-            # inference-time mini-optimization: only forward the lm_head on the very last position
-            out = self.lm_head(x) # note: using list [-1] to preserve the time dim
-            loss = None
+            b,s,f = out.shape
+            out = out.view(b,s,self.config.num_atoms, self.config.num_features)
+            #relu everything!
+            out = F.sigmoid(out)
+            if targets is not None:
+                loss = self.custom_mse_loss(out, targets)
 
         return out, loss
 
@@ -446,11 +444,13 @@ class GPT(nn.Module):
         for i in range(max_new_tokens):
             # if the sequence context is growing too long we must crop it at block_size
             chunks_cond = chunks if chunks.size(1) <= self.config.block_size else chunks[:, -self.config.block_size:]
-            # forw/ard the model to get the logits for the index in the sequence
+            # forward the model to get the logits for the index in the sequence
             # print("chunks",chunks_cond.shape)
             output, _ = self(chunks_cond)
-            # print("output",output.shape)
+            # print("output",output.cpu().numpy())
             if self.config.logit_loss:
+                #logit loss 
+                #comes out at vocab size logits + mags + phases (end to end)
                 split = self.config.vocab_size
                 #just the last in the sequence
                 logits = output[:,-1,:split]
@@ -465,20 +465,26 @@ class GPT(nn.Module):
                 chunk_next = torch.cat((idx_next, coef), dim=1).unsqueeze(0)
                 # print("chunk_next",chunk_next.shape)
                 if self.config.conv_input:
-                    #need to roll out to 2d
+                    #normalise indices
+                    chunk_next[:,:,::3] /= self.config.vocab_size
+                    #Turn to 2d for conv input on next pass
                     chunk_next = chunk_next.view(3, self.config.num_atoms)
                     chunk_next = chunk_next.t().unsqueeze(0).unsqueeze(0)
             else:
+                #mse loss
+                #comes out as normalised indexes + mags + phases (end to end)
                 split = self.config.num_atoms
                 #just the last in the sequence
                 chunk_next = output[:,-1,:].unsqueeze(0)
                 if self.config.conv_input:
+                    #Turn to 2d for conv input on next pass
                     chunk_next = chunk_next.view(3, self.config.num_atoms)
                     chunk_next = chunk_next.t().unsqueeze(0).unsqueeze(0)
                 else:
-                    #need to unnormalise
-                    chunk_next[:,:,0] = torch.floor(chunk_next[:,:,0]*self.config.dictionary_size)
+                    #need to unnormalise for embed input on next pass
+                    chunk_next[:,:,::3] = torch.floor(chunk_next[:,:,::3]*self.config.vocab_size)
             
+            # print("chunk_next",chunk_next.shape)
             chunks = torch.cat((chunks, chunk_next), dim=1)
         
         return chunks

sample.py
@@ -19,7 +19,7 @@ import soundfile as sf
 init_from = 'resume' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
 out_dir = 'out_mp' # ignored if init_from is not 'resume'
 start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
-num_samples = 1 # number of samples to draw
+num_samples = 3 # number of samples to draw
 max_new_tokens = 500 # number of tokens generated in each sample
 temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
 top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
@@ -102,11 +102,11 @@ def get_batch(split):
    
     x = x[:,:,:,:config["num_features"]]
     #normalise into the range 0-1 (from -pi - pi)
-    x[:,:,:,2] += torch.pi
-    x[:,:,:,2] /= (2*torch.pi)
+    x[:,:,:,2::3] += torch.pi
+    x[:,:,:,2::3] /= (2*torch.pi)
     if config["conv_input"]:
         #normalise into the range 0-1 (0 - dictionary_size)
-        x[:,:,:,0] /= (config["dictionary_size"])
+        x[:,:,:,::3] /= (config["dictionary_size"])
     else :
         #flatten [100x3 into 300]
         x = x.view(x.size(0), x.size(1), -1)
@@ -115,8 +115,8 @@ def get_batch(split):
 
     y = y[:,:,:,:config["num_features"]]
     #normalise into the range 0-1 (from -pi - pi)
-    y[:,:,:,2] += torch.pi
-    y[:,:,:,2] /= (2*torch.pi)
+    y[:,:,:,2::3] += torch.pi
+    y[:,:,:,2::3] /= (2*torch.pi)
 
     if config["logit_loss"]:
         #flatten [100x3 into 300]
@@ -132,7 +132,7 @@ def get_batch(split):
 # model
 if init_from == 'resume':
     # init from a model saved in a specific directory
-    ckpt_path = os.path.join(out_dir, 'ckpt_conv_input_logit_loss.pt')
+    ckpt_path = os.path.join(out_dir, 'ckpt_conv_input_mse_loss_2.pt')
     checkpoint = torch.load(ckpt_path, map_location=device)
     print("resume from checkpoint")
     gptconf = GPTConfig(**checkpoint['model_args'])
@@ -189,8 +189,9 @@ with torch.no_grad():
             #trim off seed
             y = y[config["block_size"]:]
             print(y.shape)
-            #embed input (end to end)
             if not config["conv_input"]:
+                #embed input 
+                # full indexes + mags + phases end to end 
                 interleaved = torch.empty_like(y)
                 indices = torch.arange(y.shape[1]) % 3
                 for i in range(3):
@@ -198,18 +199,18 @@ with torch.no_grad():
                     interleaved[:,indices==i] = y[:,i*n:(i+1)*n]
                 y = interleaved
             else:
-            #conv input (2d)
+                #conv input (2d)
+                # normalised indexes in a 2D tensor
                 interleaved = torch.zeros((y.shape[0],y.shape[1]*y.shape[2]), device=y.device)
                 indices = torch.arange(interleaved.shape[1]) % 3
                 for i in range(3):
                     n = config["num_atoms"]
                     interleaved[:,indices==i] = y[:,:,i]
                 y = interleaved
-            #unnormalise phase
-            y[:,2::3] = (y[:,2::3]*(2*torch.pi))-torch.pi
-            if not config["logit_loss"]:
                 #rediscretize to index (from 0-1)
                 y[:,::3] = torch.floor(y[:,::3]*config["dictionary_size"])
+            #unnormalise phase
+            y[:,2::3] = (y[:,2::3]*(2*torch.pi))-torch.pi
             #we get indexes and mags and phases, libltfat wants sparse complex coefficients 
             np.savetxt("output" + str(k) + ".csv", y.cpu().numpy(), delimiter=',',fmt='%.5f')
             

train_mp.py
@@ -168,20 +168,19 @@ def get_batch(split):
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([torch.from_numpy((data[i:i+block_size]).astype(np.float32)) for i in ix])
     y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.float32)) for i in ix]) 
-    
     if device_type == 'cuda':
         # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
         x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
     else:
         x, y = x.to(device), y.to(device)
-
+   
     x = x[:,:,:,:config["num_features"]]
     #normalise into the range 0-1 (from -pi - pi)
-    x[:,:,:,2] += torch.pi
-    x[:,:,:,2] /= (2*torch.pi)
+    x[:,:,:,2::3] += torch.pi
+    x[:,:,:,2::3] /= (2*torch.pi)
     if config["conv_input"]:
         #normalise into the range 0-1 (0 - dictionary_size)
-        x[:,:,:,0] /= (config["dictionary_size"])
+        x[:,:,:,::3] /= (config["dictionary_size"])
     else :
         #flatten [100x3 into 300]
         x = x.view(x.size(0), x.size(1), -1)
@@ -190,8 +189,8 @@ def get_batch(split):
 
     y = y[:,:,:,:config["num_features"]]
     #normalise into the range 0-1 (from -pi - pi)
-    y[:,:,:,2] += torch.pi
-    y[:,:,:,2] /= (2*torch.pi)
+    y[:,:,:,2::3] += torch.pi
+    y[:,:,:,2::3] /= (2*torch.pi)
 
     if config["logit_loss"]:
         #flatten [100x3 into 300]